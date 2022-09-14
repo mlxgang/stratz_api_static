972cc3811094dbb4abff5eb529d349aa0f2ed828
mlxgangg@gmail.com <E1gO56GS>
2022-09-14 13:10:10 +0300

    refactor game result, added item icon

--- src/utils/getHeroName.ts
+++
@@ -1,8 +0,0 @@
-import heroes from '../assets/heroes';
-import { IHero } from '../interfaces/hero.interface';
-
-export function getHeroName(id: Number) {
-    heroes.find((hero: IHero) => {
-        return hero.id === id
-    })
-}
