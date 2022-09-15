df8e576a9e80d17417ee5a570712e213a47d35f1
mlxgangg@gmail.com <E1gO56GS>
2022-09-15 16:33:22 +0300

    start to inject Vue Router

---
+++ src/components/TheMatch.vue
@@ -0,0 +1,10 @@
+<script seutp lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+const id = route.params.id
+</script>
+
+<template>
+    <div>{{id}}</div>
+</template>
